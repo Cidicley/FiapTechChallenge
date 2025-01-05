8199d72d33737158e96b53d1dab2df235ed95a18
Cidicley Rodrigues <33094500+Cidicley@users.noreply.github.com>
2025-01-05 18:11:19 -0300

    Update Usuario.cs
    Ajustes na propriedade

--- Core/Models/Usuario.cs
+++ Core/Models/Usuario.cs
@@ -1,4 +1,4 @@
-﻿using Core.Entity;
+using Core.Entity;
 using Core.Enums;
 
 namespace Core.Models
@@ -6,16 +6,16 @@ namespace Core.Models
     // Lista que vamos usar em memória para simular a consulta de usuários cadastrados
     public static class ListaUsuario
     {
-        public static IList<Usuario> Usuarios { get; set; }
+        public static IList<Usuario>? Usuarios { get; set; }
     }
 
     public class Usuario
     {
         public int Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
         public int Telefone { get; set; }
         public TipoPemissaoSistema PermissaoSistema { get; set; }
-        public RegiaoContato Regiao { get; set; }
+        public RegiaoContato? Regiao { get; set; }
     }
 }
